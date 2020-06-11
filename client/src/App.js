356f961efb9baca14c0735476350412a7e1f3458
nathannlu <nathanlu_@hotmail.com>
2020-06-10 18:37:53 -0700

    Front-end back-end integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
+const App = () => {
+	axios.get('/api/users/').then(res => console.log(res));
+  
+	return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+			Hi this is home
+		</div>
   );
 }
 
