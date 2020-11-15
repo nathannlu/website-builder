88665e07d14c4a87e138ca2f3931718d334cfb8a
CanFISHfly1 <nathanlu_@hotmail.com>
2020-11-15 11:13:13 -0800

    Changed signup flow to production

--- client/src/components/pages/Signup/index.js
+++ client/src/components/pages/Signup/index.js
@@ -4,38 +4,28 @@ import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import SignupForm from '../../organisms/SignupForm';
-import Success from '../../organisms/SignupForm/Success';
 import SignupWrapper from '../../templates/SignupWrapper';
 
 const Signup = props => {
-	const [onSuccess, setOnSuccess] = useState({success:true});
 	// Check if user is logged in
 	useEffect(() => {
 		if(props.auth.isAuthenticated) {
-			props.history.push('/dashboard');
+			props.history.push('/dashboard/welcome');
 		}
 	}, [props.auth])
 
 	return (
 		<SignupWrapper>
-			{
-				onSuccess.success ? (
-					<Success />
-				) : (
-					<>
-						<h1>
-							Welcome to Agentsquare 
-						</h1>
-						<p className="mb-3">
-							Find your people. Engage your customers. Build your brand. Do it all with Agentsquare’s Web Building Platform. 
-						</p>
-						<p>
-							Already have an account? <Link className="link" to="/login">Log in</Link>
-						</p>
-						<SignupForm history={props.history} />
-					</>
-				)
-			}
+			<h1>
+				Welcome to Agentsquare 
+			</h1>
+			<p className="mb-3">
+				Find your people. Engage your customers. Build your brand. Do it all with Agentsquare’s Web Building Platform. 
+			</p>
+			<p>
+				Already have an account? <Link className="link" to="/login">Log in</Link>
+			</p>
+			<SignupForm history={props.history} />
 		</SignupWrapper>
 	)
 };
